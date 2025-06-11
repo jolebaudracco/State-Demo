7f385faf51304533ee0e3776ca86aec90dc4ce2b
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-06-11 12:54:26 -0300

    component emoji clicker to interact with the emojis

---
+++ state-demo/src/Component/EmojiClicker.jsx
@@ -0,0 +1,30 @@
+import {v4 as uuid} from "uuid"
+import { useState } from "react"
+
+function randomEmoji() {
+    const choices = ["😬", "😘", "😭", "🙄","🤪","🤢"]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+export default function EmojiClicker(){
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
+    const addEmoji = () => {
+        setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: randomEmoji() }]);
+    };
+
+    const deleteEmoji = (id) => {
+        setEmojis(prevEmojis => {
+            return prevEmojis.filter(e => e.id !== id)
+        })
+    }
+
+    return (
+        <div>
+            {emojis.map((e) => (
+                <span onClick={() => deleteEmoji(e.id)} key={e.id} style={{fontSize: "4rem"}}>{e.emoji}</span>
+            ))}
+            <button onClick={addEmoji}>Add Emoji</button>
+        </div>
+    );
+}
+
