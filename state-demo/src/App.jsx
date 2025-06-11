ba4af64343eabdec2a39360e94bb27e03a65cdd4
JoleBaudracco <85041450+jolebaudracco@users.noreply.github.com>
2025-06-11 10:43:02 -0300

    adding three new components to my app

---
+++ state-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Counter from './Component/Counter'
+import Dumbo from './Component/Dumbo'
+import ScoreKeeper from './Component/ScoreKeeper'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>State Demo!</h1>
+      <Counter/>
+      <Dumbo/> 
+      <ScoreKeeper/>
+    </>
+  )
+}
+
+export default App
